b12dc77fac76c3bf7b74646f3b9e02137fe52408
ehmed007 <ehmedmughal@gmail.com>
2024-09-30 01:58:11 -0700

    remove python file and introducting java file

--- QA.py
+++
@@ -1,1 +0,0 @@
-print("this is QA Branch")
---
+++ check.java
@@ -0,0 +1,8 @@
+
+
+public class check {
+
+    public static void main(String[] args) {
+        System.out.println("hello world from java code");
+    }
+}
