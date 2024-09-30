2638c8d5173f9b31d9dd9ef928e107e3f85bd631
ehmed007 <ehmedmughal@gmail.com>
2024-09-30 09:24:35 -0700

    commit

--- check.java
+++ check.java
@@ -1,8 +1,10 @@
 
-
 public class check {
 
     public static void main(String[] args) {
         System.out.println("hello world from java code");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
